28e14842605f7ad36a8bebbfa6e4732e0b7a8b42
sa1k <svsvsk@gmail.com>
2023-10-08 14:20:58 -0400

    small changes

--- web scraping.py
+++ web scraping.py
@@ -4,6 +4,9 @@ import time
 import os
 import pandas as pd
 
+
+#path for first time file
+flag_file_path = 'first_time.flag'
 # Function to scrape the website
 def scrape_website(url):
     try:
@@ -19,15 +22,13 @@ def scrape_website(url):
                 #file.write(str(soup.prettify()))
 
             
-            #checks whether this is the first time that the program is being run 
-            flag_file_path = 'first_time.flag'
-            
             #creates dataframe
             trade_df = pd.DataFrame()
             
+            #TODO: rethink how non first operation is working
             #if not first time
             if os.path.exists(flag_file_path):
-                print('not first time')
+                #print('not first time')
                 trades = soup.find_all("tr")
                 for element in trades[1:]:
                     name = element.find("h3", class_ = 'q-fieldset politician-name').next.next
@@ -37,13 +38,10 @@ def scrape_website(url):
                     trade_size = element.find('div', class_ = 'q-range-icon-wrapper').find('span', class_ = 'q-label').next
                     to_append = pd.DataFrame({'Congress Person': [name],  'Stock name': [stock], 'Ticker': [ticker], 'Buy/Sell': [direction], 'Size of Trade': [trade_size]})
                     trade_df = pd.concat([trade_df , to_append], ignore_index=True).drop_duplicates()
-            
-                    
-                    
+
+
             #if first time
             else:
-                with open(flag_file_path, 'w') as flag_file:
-                    flag_file.write("Flag indicating the program has run.")
                 trades = soup.find_all("tr")
                 for element in trades[1:]:
                     name = element.find("h3", class_ = 'q-fieldset politician-name').next.next
@@ -53,10 +51,12 @@ def scrape_website(url):
                     trade_size = element.find('div', class_ = 'q-range-icon-wrapper').find('span', class_ = 'q-label').next
                     to_append = pd.DataFrame({'Congress Person': [name],  'Stock name': [stock], 'Ticker': [ticker], 'Buy/Sell': [direction], 'Size of Trade': [trade_size]})
                     trade_df = pd.concat([trade_df , to_append], ignore_index=True)
-            
+
+            trade_df = trade_df[~trade_df.apply(lambda row: row.astype(str).str.contains('N/A').any(), axis=1)]
             print(trade_df)
             print("Scraping successful")
-        
+            return trade_df
+
         #handles website being down    
         else:
             print(f"Failed to retrieve data. Status code: {response.status_code}")
@@ -65,13 +65,33 @@ def scrape_website(url):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def execute_trade(trades):
+    max_limit = 50
+    min_limit = 5
+    to_execute = pd.DataFrame()
+    #if not first set of trades
+    if os.path.exists(flag_file_path):
+        print("hi :)")
+
+    #if first time set up trades
+    else:
+        with open(flag_file_path, 'w') as flag_file:
+            flag_file.write("Flag indicating the program has run.")
+        for element in trades.index:
+            print(trades['Ticker'][element])
+
+
 # URL of the website to scrape
 url = "https://www.capitoltrades.com/trades"
 
 # Set the interval for scraping
-scraping_interval = 3 * 60 * 60  # 3 hours
+scraping_interval = 8 * 60 * 60  # 8 hours
 
 #continous website scraping
 while True:
-    scrape_website(url)
+    trades = scrape_website(url)
+    execute_trade(trades)
+    #print(trades)
     time.sleep(scraping_interval)
+
+
