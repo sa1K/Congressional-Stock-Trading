13679f9dc051c687bf880feb302cd7848fafa7cd
sa1k <svsvsk@gmail.com>
2023-10-31 22:51:52 -0400

    can login now

--- trader.py
+++ trader.py
@@ -1,20 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import selenium.common.exceptions
 import config
+from selenium.webdriver.chrome.options import Options
 
 
-def login():
-    driver = webdriver.Firefox()
-    # driver.get("https://login.fidelity.com/ftgw/Fas/Fidelity/RtlCust/Login/Init")
-    # driver.find_element(By.ID, "userId-input")
-    # username = driver.find_element(By.ID, "userId-input")
-    # password = driver.find_element(By.ID, "password")
+def login(driver):
     driver.get("https://robinhood.com/login")
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(1)
     driver.find_element(By.NAME, "username").send_keys(config.USERNAME)
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(1)
     driver.find_element(By.NAME, "password").send_keys(config.PASSWORD)
+    driver.implicitly_wait(1)
+    for i in range(10):
+        try:
+            driver.find_element(
+                By.XPATH,
+                '//*[@id="react_root"]/div[1]/div[2]/div/div/div/div[2]/div/form/div/div[3]/label/div/div/div',
+            ).click()
+            break
+        except selenium.common.exceptions.NoSuchElementException as e:
+            print("Retry in 1 second")
+            driver.implicitly_wait(1)
+    else:
+        raise e
+    driver.implicitly_wait(1)
+    driver.find_element(By.XPATH, '//*[@id="submitbutton"]/div/button').click()
 
 
 if __name__ == "__main__":
-    login()
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=chrome_options)
+    login(driver)
