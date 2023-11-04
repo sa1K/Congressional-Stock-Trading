3afad4324e39d9c2a6d52370ccdb837b192e6cee
sa1k <svsvsk@gmail.com>
2023-11-04 12:06:04 -0400

    can select whether buying or selling

--- trader.py
+++ trader.py
@@ -8,11 +8,11 @@ from selenium.webdriver.common.keys import Keys
 
 def login(driver):
     driver.get("https://robinhood.com/login")
-    driver.implicitly_wait(1)
+    driver.implicitly_wait(15)
     driver.find_element(By.NAME, "username").send_keys(config.USERNAME)
-    driver.implicitly_wait(1)
+    driver.implicitly_wait(15)
     driver.find_element(By.NAME, "password").send_keys(config.PASSWORD)
-    driver.implicitly_wait(1)
+    driver.implicitly_wait(15)
     for i in range(10):
         try:
             driver.find_element(
@@ -27,13 +27,27 @@ def login(driver):
         raise e
     driver.implicitly_wait(1)
     driver.find_element(By.XPATH, '//*[@id="submitbutton"]/div/button').click()
-
-
-def trade(driver):
     driver.implicitly_wait(60)
-    driver.find_element(By.XPATH, '//*[@id="downshift-0-input"]').send_keys("WMT")
-    driver.implicitly_wait(1)
-    driver.find_element(By.XPATH, '//*[@id="downshift-0-input"]').send_keys(Keys.RETURN)
+
+
+def trade(driver, stock, amount, direction):
+    driver.implicitly_wait(60)
+    driver.find_element(By.XPATH, '//*[@id="downshift-0-input"]').send_keys(stock)
+    driver.implicitly_wait(15)
+    driver.get("https://robinhood.com/stocks/" + stock + "?source=search")
+    if direction == "sell":
+        print("selling")
+        driver.implicitly_wait(30)
+        driver.find_element(
+            By.XPATH,
+            '//*[@id="sdp-ticker-symbol-highlight"]/div[1]/form/div[1]/div/div[1]/div/div/div[2]/div/div/div/div',
+        ).click()
+        print("done")
+    elif direction == "buy":
+        print("buying")
+        driver.implicitly_wait(30)
+        webdriver.ActionChains(driver).send_keys("b")
+        print("done")
 
 
 if __name__ == "__main__":
@@ -41,4 +55,4 @@ if __name__ == "__main__":
     chrome_options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=chrome_options)
     login(driver)
-    trade(driver)
+    trade(driver, "LLY", 1, "sell")
